9600469432e365cd01f36a2942dd1bb2e814dc43
angus.a.lin@gmail.com <angus.a.lin@gmail.com>
2017-04-08 20:35:05 -0400

    Added Hex Display for Left Auto, Right Auto, Finish, and Choose

---
+++ hex_L_auto.s
@@ -0,0 +1,24 @@
+.equ HEX0TO3, 0xFF200020
+.equ HEX4TO5, 0xFF200030
+
+.global hex_auto
+
+hex_auto:
+	
+	addi sp, sp, -8
+	stw r8, 0(sp)
+	stw r9, 4(sp)
+
+	#HEX code to write AUTO 
+	movia r8, HEX0TO3
+	movia r9, 0b01110111001111100111100000111111
+	stwio r9, 0(r8)
+	movia r8, HEX4TO5
+	movia r9, 0b0011100000000000 
+	stwio r9, 0(r8)
+
+	ldw r9, 0(sp)
+	ldw r8, 4(sp)
+	addi sp, sp, 8
+
+ret
---
+++ hex_choose.s
@@ -0,0 +1,23 @@
+.equ HEX0TO3, 0xFF200020
+.equ HEX4TO5, 0xFF200030
+
+.global hex_manual
+
+hex_manual:
+
+	addi sp, sp, -8
+	stw r8, 0(sp)
+	stw r9, 4(sp)
+	
+	#HEX code to write CHOOSE
+	movia r8, HEX0TO3
+	movia r9, 0b00111111001111110110110101111001
+	stwio r9, 0(r8)
+	movia r8, HEX4TO5
+	movia r9, 0b0011100100111001 
+	stwio r9, 0(r8)
+
+	ldw r9, 0(sp)
+	ldw r8, 4(sp)
+	addi sp, sp, 8
+ret
